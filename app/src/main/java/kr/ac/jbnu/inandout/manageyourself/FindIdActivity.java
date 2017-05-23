d2f46b9ce239cdb21132b8c346a55642289d852a
Hong <rlwns012@gmail.com>
2017-05-23 18:57:25 +0900

    바뀐 xml 적용 및 액티비티 기본뼈대 구축 사용자의 기간 받기 성공

--- app/src/main/java/kr/ac/jbnu/inandout/manageyourself/FindIdActivity.java
+++ app/src/main/java/kr/ac/jbnu/inandout/manageyourself/FindIdActivity.java
@@ -31,26 +31,26 @@ public class FindIdActivity extends Activity {
         setContentView(R.layout.activity_findid);
         etName = (EditText) findViewById(R.id.editText4);
         etBirth = (EditText) findViewById(R.id.editText5);
-        foundid = (TextView)findViewById(R.id.foundId);
+        foundid = (TextView) findViewById(R.id.foundId);
 
         soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        sound = soundPool.load(this,R.raw.button,1);
+        sound = soundPool.load(this, R.raw.button, 1);
     }
 
     public void getIdButtonClicked(View view) {
 
-        soundPool.play(sound,1,1,0,0,1);
+        soundPool.play(sound, 1, 1, 0, 0, 1);
 
         String name = etName.getText().toString();
         String birth = etBirth.getText().toString();
         UserDBOpenHelper udbHelper = new UserDBOpenHelper(FindIdActivity.this);
         ArrayList<String> ids = udbHelper.findId(name, birth);
         Iterator iterator = ids.iterator();
-        String book1 ="";
-        if(!iterator.hasNext())           Toast.makeText(FindIdActivity.this,
-                "이름과 생년월일을 다시 확인해주세요", Toast.LENGTH_SHORT).show();
-        while (iterator.hasNext() ){
-            book1 = book1 + iterator.next() +"  ";
+        String book1 = "";
+        if (!iterator.hasNext())
+            Toast.makeText(FindIdActivity.this, "이름과 생년월일을 다시 확인해주세요", Toast.LENGTH_SHORT).show();
+        while (iterator.hasNext()) {
+            book1 = book1 + iterator.next() + "  ";
         }
         foundid.setText(book1);
 
@@ -58,7 +58,7 @@ public class FindIdActivity extends Activity {
 
 
     public void gotoLoginButtonClicked(View view) {
-        soundPool.play(sound,1,1,0,0,1);
+        soundPool.play(sound, 1, 1, 0, 0, 1);
 
         finish();
     }
--- app/src/main/java/kr/ac/jbnu/inandout/manageyourself/SettingMonthActivity.java
+++ app/src/main/java/kr/ac/jbnu/inandout/manageyourself/SettingMonthActivity.java
@@ -6,37 +6,48 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by rlwns on 2017-05-21.
  */
 
-public class SettingMonthActivity extends Activity{
+public class SettingMonthActivity extends Activity {
 
     private User user;
     private SharedPreferences checkMonth;
+    private EditText days;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setmonth);
 
+        days = (EditText) findViewById(R.id.month);
+
         Intent intent = getIntent();
         user = (User) intent.getSerializableExtra("user"); // 로그인에서 받아온 user 정보를 넘겨 받는다.
 
-        checkMonth =getSharedPreferences("checkMonth", Activity.MODE_PRIVATE);
+        checkMonth = getSharedPreferences("checkMonth", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = checkMonth.edit();
-        editor.putBoolean(user.getId(),true);
+        editor.putBoolean(user.getId(), true);
         editor.commit();
 
     }
 
-    public void setMonthConfirm(View view){
+    public void setMonthConfirm(View view) {
         //기간 입력시에 알맞은 값을 넣었는지 확인
-
-        Intent intent = new Intent(this, MenuActivity.class);
-        intent.putExtra("user", user);
-        startActivity(intent);
-        finish();
+        String daystr = days.getText().toString();
+        int dayint = Integer.parseInt(daystr);
+        if(dayint < 30 && dayint>1865){
+            Toast.makeText(SettingMonthActivity.this, "설정할 수 있는 기간은 최소 30일 최대 1865일 입니다.", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent(this, MenuActivity.class);
+            user.setDayCount(dayint);
+            intent.putExtra("user", user);
+            startActivity(intent);
+            finish();
+        }
     }
 }
